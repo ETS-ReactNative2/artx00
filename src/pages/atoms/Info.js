f02fa6db14256ad8b2e18853bc2373064ce413e7
sarahvc <sarahvc@qq.com>
2019-05-10 10:40:56 -0400

    update

--- src/pages/atoms/Info.js
+++ src/pages/atoms/Info.js
@@ -7,7 +7,6 @@ const Info = ({content}) => {
     return (
         <button className='align-baseline ml-1 position-relative artx-help-icon'>
             <img src={helpicon} alt='info tip'/>
-            <span className='position-absolute'>This is the difference between the hardcap and the current price. It will serve as the timer for the auction. The hardcap starts high and decreases over time. The auction ends once the accumulated sales of Genesis shares reach or exceed the hardcap.</span>
         </button>
     )
 };
